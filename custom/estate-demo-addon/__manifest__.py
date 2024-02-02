3e69cf8afc9569ef649167895c2d01ac087fd946
m3r3nix <m3r3nix@protonmail.com>
2024-02-02 15:37:15 +0100

    Create v15 config

---
+++ custom/estate-demo-addon/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Real Estate",
+    "depends": [
+        "base",
+    ],
+    "version": "15.0.0.1",
+    "application": "True",
+    "category": "Sales",
+    "description": "Manage real estate offerings",
+    "license": "LGPL-3",
+    "data": [
+        "security/ir.model.access.csv",
+
+        "views/estate_property_views.xml",
+        "views/estate_menus.xml",
+    ],
+}
