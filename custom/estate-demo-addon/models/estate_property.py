465c84466f1ddbe7db866b68c1d6e9fdc857fef4
m3r3nix <m3r3nix@protonmail.com>
2024-10-27 14:57:37 +0100

    Create Odoo 18 configuration

--- custom/estate-demo-addon/models/estate_property.py
+++ custom/estate-demo-addon/models/estate_property.py
@@ -6,22 +6,23 @@ class EstateProperty(models.Model):
     _description = "Real Estate Property details"
 
     name = fields.Char('Name', required=True)
-    description = fields.Text()
-    postcode = fields.Char()
-    date_availability = fields.Date(default=fields.Date.today() + relativedelta(months=3), copy=False)
-    expected_price = fields.Float(required=True)
-    selling_price = fields.Float(readonly=True, copy=False)
-    bedrooms = fields.Integer(default=2)
-    living_area = fields.Integer()
-    facades = fields.Integer()
-    garage = fields.Boolean()
-    garden = fields.Boolean()
-    garden_area = fields.Integer()
+    description = fields.Text('Description')
+    postcode = fields.Char('Postcode')
+    date_availability = fields.Date('Available From', default=fields.Date.today() + relativedelta(months=3), copy=False)
+    expected_price = fields.Float('Expected Price', required=True)
+    selling_price = fields.Float('Selling Price', readonly=True, copy=False)
+    bedrooms = fields.Integer('Bedrooms', default=2)
+    living_area = fields.Integer('Living Area (m²)')
+    facades = fields.Integer('Facades')
+    garage = fields.Boolean('Garage')
+    garden = fields.Boolean('Garden')
+    garden_area = fields.Integer('Garden Area (m²)')
     garden_orientation = fields.Selection(
-        string='Orientation',
+        string='Garden Orientation',
         selection=[('north', 'North'),('south', 'South'),('east', 'East'),('west', 'West')])
     state = fields.Selection(
         string='Status',
         selection=[('new', 'New'), ('offer_received', 'Offer Received'), ('offer_accepted', 'Offer Accepted'), ('sold', 'Sold'), ('canceled', 'Canceled')],
         default='new')
     active = fields.Boolean(default=True)
+    
